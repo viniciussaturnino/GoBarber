ba7fc07714a42786ed17bae1bed9571a318d5524
viniciussaturnino <viniciussaturnino07@hotmail.com>
2020-08-01 16:48:22 -0300

    Config nodemailer for notify provider when client cancel appoinment

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    secure: false,
+    auth: {
+        user: '950ee7cbe9c8c9',
+        pass: '39e2d074014a40',
+    },
+    default: {
+        from: 'Equipe GoBarber <noreply@gobarber.com>',
+    },
+};
